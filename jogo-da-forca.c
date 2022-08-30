2192036c32f91ccf16277433a10247579bef923e
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-08-29 23:26:15 -0300

    lendo palavra

---
+++ jogo-da-forca.c
@@ -0,0 +1,23 @@
+/*
+
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+#define TAM 50
+
+int main(){	
+	char string[TAM];
+	
+	//lendo palvra
+	printf("entre com uma palavra: ");
+	fgets(string, TAM, stdin);
+	system("pause"); 
+	system("cls");
+	
+	
+	
+}
+
