3d10b2f753c95b5f0f28880d350e745f983487d5
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-09-02 21:34:58 -0300

    tratando dados: letra

--- jogo-da-forca.c
+++ jogo-da-forca.c
@@ -10,47 +10,24 @@
 #define TRUE 1
 #define FALSE 0
 
-int buscarCaracter(char string[], char caracter);
-void exibirPalavra(char string[], char caracter);
+
 int main(){	
-	int tentativas; 
+	
 	char string[TAM], letra;
 	
 	//lendo palvra
 	printf("entre com uma palavra: ");
 	fgets(string, TAM, stdin);
-	strlwr(string);	//converte letras maiusculas em minuscolas
-	//exibindo palavra
-	printf("A palavra escolhida e: %s", string);	
+	strlwr(string);	//converte letras maiusculas em minuscolas		
 	system("pause"); 
 	system("cls");
-	exibirPalavra(string, letra);
-	//Lendo letra a ser buscada
+
+
 	printf("\nescolha uma letra: ");
-	fflush(stdin);	//limpando buffer 
+	fflush(stdin);	//limpando buffer	
 	scanf("%c", &letra);
 	letra=tolower(letra);
-	exibirPalavra(string, letra); 
 	
-}
-int buscarCaracter(char string[], char caracter){
-	int i;
-	for(i=0;string[i];i++){
-		if(string[i]==caracter){
-			return TRUE;
-		}
-		return FALSE;
-	}
-}
-void exibirPalavra(char string[], char caracter){
-	int i;
-	printf("\tpalavra escolhida: ");
-	for(i=0;string[i];i++){
-		if(string[i]==caracter){
-			printf("%c",string[i]);
-		}else{
-			printf("*");
-		}
-	}
+		
 }
 
