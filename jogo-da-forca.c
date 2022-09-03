34efcf806995ef2d900a1314c17bc54362e1c511
rafaela-ale-oliveira <103013509+rafaela-ale-oliveira@users.noreply.github.com>
2022-09-03 10:39:47 -0300

    alterando retorno da funcao buscar

--- jogo-da-forca.c
+++ jogo-da-forca.c
@@ -1,5 +1,5 @@
 /*
-
+ta dando tudo errado 
 */
 #include<stdio.h>
 #include<stdlib.h>
@@ -21,50 +21,64 @@ int main(){
 	//lendo palvra
 	printf("entre com uma palavra: ");
 	fgets(string, TAM, stdin);
-	strlwr(string);	//converte letras maiusculas em minuscolas		
+	strlwr(string);							//converte letras maiusculas em minuscolas		
+
 	system("pause"); 
 	system("cls");
+
 	exibirPalavra(string, letra);
 
 
-	while(tentativa){
+	while(tentativa!=0){
+		
 		printf("\nescolha uma letra: ");
-		fflush(stdin);	//limpando buffer	
+		fflush(stdin);						//limpando buffer	
 		scanf("%c", &letra);
+		
 		letra=tolower(letra);
+		
 		verificarCondicao(string, letra, &tentativa);
 	}
-	printf("Fim do jogo");
+//	printf("Fim do jogo");
 		
 }
 void exibirPalavra(char string[], char caracter){
 	int i;
+	
 	printf("\tpalavra escolhida: ");
+	
 	for(i=0;string[i];i++){
 		if(string[i]==caracter){
 			printf("%c",string[i]);
+			
 		}else{
+			
 			printf("*");
 		}
 	}
 }
 int buscarCaracter(char string[], char caracter){
-	int i;
-	for(i=0;string[i];i++){
-		if(string[i]==caracter){
-			return TRUE;
-		}
+	int i, cont=0;
+	for(i=0;string[i];i++){		
+		if(caracter==string[i]){
+			cont++;
+		}		
+	}
+	if(!cont){
 		return FALSE;
 	}
+	return cont;	
 }
 void verificarCondicao(char string[], char caracter, int *cont){
 	int retorno;
+	
 	retorno= buscarCaracter(string, caracter);
+	
 	if(retorno==TRUE){
 		exibirPalavra(string, caracter);
 		printf("\nTentativas restantes: %d", *cont);
 	}else{
-		*(cont--);
+//		*(cont--);
 		exibirPalavra(string, caracter);
 		printf("\nLetra nao encontrada\n");
 		printf("Tentativas restantes: %d", *cont);	
